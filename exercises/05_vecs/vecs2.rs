7110746708ecc14cf269e055e4e378cf38ce18fb
sevenhundredbees <jb215d@gmail.com>
2026-02-03 22:10:53 -0600

    finish structs

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -2,10 +2,11 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
     for element in input {
+        output.push(element * 2)
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
     }
-
+    
     output
 }
 
--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -24,14 +24,17 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
-        // TODO: Read the tests that use this method to find out when a package
-        // is considered international.
+    fn is_international(&self) -> bool {
+        if self.sender_country != "Canada" {
+            return true;
+        }
+        self.recipient_country != "Canada"
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // TODO: Calculate the package's fees.
+        cents_per_gram * self.weight_in_grams
     }
 }
 
